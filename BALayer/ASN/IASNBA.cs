867c621ac704d44b8bb47b3870d0eff40c390928
Sowmya493 <Developer6@in.yokogawa.com>
2020-08-05 11:57:20 +0530

    Interface of BAL and DAL added.

---
+++ BALayer/ASN/IASNBA.cs
@@ -0,0 +1,15 @@
+﻿using SCMModels.ASNModels;
+using SCMModels.RFQModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALayer.ASN
+{
+    public interface IASNBA
+    {
+        Task<statuscheckmodel> CreateAsn(ASNShipmentHeaderModel model);
+    }
+}
